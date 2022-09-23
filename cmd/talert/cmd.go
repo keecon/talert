e545119b4f2cbd9a492e9978bb326147a9f968f3
iwaltgen <iwaltgen@gmail.com>
2022-09-23 17:06:36 +0900

    chore: change sub cmd file name

--- cmd/talert/cmd.go
+++ cmd/talert/cmd.go
@@ -72,3 +72,21 @@ func newWatchCmd() *cli.Command {
 		},
 	}
 }
+
+func newTestCmd() *cli.Command {
+	return &cli.Command{
+		Name:  "test",
+		Usage: "check message pattern in file",
+
+		Action: func(ctx *cli.Context) error {
+			filename := ctx.Args().First()
+			tester := internal.NewTester()
+
+			return tester.Test(filename, &internal.Config{
+				Pattern:  ctx.String("pattern"),
+				Levels:   ctx.StringSlice("level"),
+				Excludes: ctx.StringSlice("exclude"),
+			})
+		},
+	}
+}
